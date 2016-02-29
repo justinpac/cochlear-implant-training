6f3f878f52c43429aed4529d5bbd9eaa80b1aa96
Justin Pacholec <Justin Pacholec>
2016-02-28 20:45:08 -0600

    added history page, button links, formatting banner

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('cochlear',
     url(r'^$', 'views.index', name='index' ),
     url(r'^speaker/$', 'views.speaker', name='speaker' ),
+    url(r'^history/$','views.history', name = 'history'),
 )
--- views.py
+++ views.py
@@ -9,10 +9,16 @@ from cochlear.models import *
 def index(request):
 	#Render a basic page
 	context = NavigationBar.generateAppContext(request,app="cochlear",title="index", navbarName=0);
-
+	#Fancy database queries
+	context['name'] = "\"False\"";#request.user.username;
 	return render(request,'cochlear/index.html',context)
 
 def speaker(request):
 	context = NavigationBar.generateAppContext(request,app="cochlear",title="speaker", navbarName=0);
 
 	return render(request,'cochlear/speaker.html',context)
+
+def history(request):
+	context = NavigationBar.generateAppContext(request,app="cochlear",title="history", navbarName=0);
+
+	return render(request,'cochlear/history.html',context)
